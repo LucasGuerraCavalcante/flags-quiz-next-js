9fddf8578e5ead9702bf0f02f18cb2aea04a3bd1
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2021-01-28 18:55:02 -0300

    AnswerMessage

---
+++ src/components/AnswerMessage/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const AnswerMessage = styled.div`
+    display: flex;
+    justify-content: center;
+    padding-top: 15px;
+    font-size: 20px;
+`;
+
+export default AnswerMessage;
