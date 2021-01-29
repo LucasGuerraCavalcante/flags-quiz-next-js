c41118658b995c4b518a206608351849205c0483
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2021-01-29 15:26:12 -0300

    DicaDiv components

---
+++ src/components/DicaDiv/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const DicaDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 15px;
+    margin-bottom: 15px;
+    font-size: 20px;
+`;
+
+export default DicaDiv;
